020dbe1f5303b5e66ffea83f9761011780e3ae37
FAUSTINI GIULIA <giulia.faustini@izsler.it>
2025-12-15 14:17:19 +0100

    Update model code

--- Dynamic_Network_Based_Model/DynamicFMDModelingHerdRemoval.py
+++ Dynamic_Network_Based_Model/DynamicFMDModelingHerdRemoval.py
@@ -11,12 +11,12 @@ Spyder Editor
 ## Second phase: obtain an adjacency matrix for each simulation and run an agent-based 
 ### dynamic network model based on the NDlib Python library https://ndlib.readthedocs.io/en/latest/
 
-import networkx as nx
-import ndlib.models.epidemics as ep
-import pandas as pd
-import numpy 
-import random 
-from scipy.spatial.distance import cdist
+# Python (version 3.11.5)
+import networkx as nx                      # NetworkX library
+import ndlib.models.epidemics as ep        # Ndlib library
+import pandas as pd                        # Pandas library 
+import numpy                               # NumPy library
+from scipy.spatial.distance import cdist   # SciPy library 
 
 # Import database of farms with attributes and coordinates
 
@@ -41,15 +41,17 @@ kernel_matrix = power_law_kernel(distance_matrix, 1, 2.500, 2.3)
 
 numpy.fill_diagonal(kernel_matrix, 0.0)
 
-# 1000 simulatiions; results populate the outputtrends list
+# 1000 simulations; results populate the outputtrends list
 outputtrends =[]
 for y in range(1000):
-    ###############################################################
+# Infectivity and susceptibility parameters from: 
+# (Pesciaroli et al. 2025: https://doi.org/10.3390/ani15030386)
 # Infectivity parameters of the farms type
-    mydata['sus'] = numpy.random.beta(90,10, 3074)
-    mydata['bos'] = numpy.random.beta(45,55, 3074)
-    mydata['susmall'] = numpy.random.beta(45,55, 3074)
-    mydata['bosmall'] = numpy.random.beta(25,75, 3074)
+# Farm included in the study n=3074. In case of a new dataset, use N = len(mydata)
+    mydata['sus'] = numpy.random.beta(90,10, 3074)     # Large swine farms
+    mydata['bos'] = numpy.random.beta(45,55, 3074)     # Large cattle farms
+    mydata['susmall'] = numpy.random.beta(45,55, 3074) # Small swine farms
+    mydata['bosmall'] = numpy.random.beta(25,75, 3074) # Small cattle farms 
 # Infectivity, four levels 
     mydata['release_lev_if'] = 0
     mydata.loc[mydata['tipo'] == 2, 'release_lev_if'] = mydata['sus']
@@ -58,12 +60,12 @@ for y in range(1000):
     mydata.loc[mydata['tipo'] == 3, 'release_lev_if'] = mydata['bosmall']
 #####################################################################################
 # Susceptibility parameters of the farms type 
-    mydata['expbos'] = numpy.random.beta(90,10, 3074)
-    mydata['expsus'] = numpy.random.beta(6, 94, 3074)
+    mydata['expbos'] = numpy.random.beta(90,10, 3074) # Cattle farms 
+    mydata['expsus'] = numpy.random.beta(6, 94, 3074) # Swine farms 
 ######## Susceptibility, two levels 
     mydata['exposure_lev_if'] = 0
-    mydata.loc[mydata['species'] == 2, 'exposure_lev_if'] = mydata['expsus']
-    mydata.loc[mydata['species'] == 1, 'exposure_lev_if'] = mydata['expbos']
+    mydata.loc[mydata['species'] == 2, 'exposure_lev_if'] = mydata['expsus'] # 2=swine
+    mydata.loc[mydata['species'] == 1, 'exposure_lev_if'] = mydata['expbos'] # 1=bovine
 #################### Multiply element wise by the kernel matrix #############
 # Ensure these are numpy arrays
     release_lev_if = mydata.release_lev_if.to_numpy()[:, None] # shape (N, 1)
@@ -73,10 +75,13 @@ for y in range(1000):
     mymatrelease = mymat2 * release_lev_if
     mymatexp = mymat2 * exposure_lev_if
     mymatrelexp = mymatrelease * mymatexp 
-#############################################################################################################
+###################################################################################################################
 # For each simulations, an edge is obtained if the probability values in mymatrelexp > a fixed random number between 0 and 1  
-    mymat3 = numpy.where(numpy.random.random(1)[0] < mymatrelexp, 1, 0)  
-###############################################################################################################
+# By the following code we obtain a fixed threshold for each of the 1000 simulations
+# in this way, we consider uncertainty in the threshold of the transmission
+# probability of mymatrelexp resulting from kernel distance and farm type   
+    mymat3 = numpy.where(numpy.random.random(1)[0] < mymatrelexp, 1, 0) 
+####################################################################################################################
 # Build the farm network
 # ----------------------------
     G = nx.DiGraph(numpy.array(mymat3))
@@ -86,10 +91,10 @@ for y in range(1000):
 #    g2.remove_nodes_from(random.sample(list(G.nodes()), 1500))
 # Alternative hard removal criterium: degree 
     top_nodes = sorted(dict(G.degree).items(),key=lambda x:x[1],reverse=True)
-    top1500 = top_nodes[0:1499] # first 1500 herds in terms of degree as an index of connectedness 
+    top1500 = top_nodes[0:1500] # first 1500 herds in terms of degree as an index of connectedness 
     top1500names = [None] * 1500  # list of 1500 with None 
 # Populate the list  
-    for i in range(0,1499):
+    for i in range(0,1500):
         top1500names[i] = top1500[i][0]
 # Remove 1500 herds (nodes) with maximum degree 
     g2=G.copy()
@@ -105,6 +110,7 @@ for y in range(1000):
     import ndlib.models.ModelConfig as mc
 #################################################
 # Model Configuration
+# (Pesciaroli et al. 2025: https://doi.org/10.3390/ani15030386)
     config = mc.Configuration()
     config.add_model_parameter('beta', 0.25)
     config.add_model_parameter('gamma', 0.1)
@@ -128,7 +134,7 @@ for y in range(1000):
     trends_infected_nodes = trends[0]["trends"]["node_count"][1]
 # append the result of each new simulation to the list outputtrends    
     outputtrends.append(trends_infected_nodes)
-df1700 = pd. DataFrame(outputtrends)
-print(df1700.head())
-df1700.to_csv('path to .csv')
+df1500removed = pd. DataFrame(outputtrends)
+print(df1500removed.head())
+df1500removed.to_csv('path to .csv')
 # Results can be exported for further analysis and graphical representation
